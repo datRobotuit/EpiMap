b609e04287785313c3068eae4a6f09c6654132e2
Le Minh Khoi <leminhkhoi36@gmail.com>
2025-06-11 15:45:34 +0700

    feature: add endpoints

--- src/services/epidemiology.endpoints.js
+++
@@ -1,8 +0,0 @@
-const EpidemiologyEndpoints = {
-  getPatients: "/patients",
-  addPatient: "/patients",
-  deletePatientsByIds: "/patients/delete-many",
-  updatePatient: "/patients",
-};
-
-export default EpidemiologyEndpoints;
