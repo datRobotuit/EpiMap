b4403c7ce1a86c33a58970c7bb7bac773b7855fc
Le Minh Khoi <leminhkhoi36@gmail.com>
2025-06-11 15:02:50 +0700

    feature: add api

---
+++ src/apis/epidemiologyApi.js
@@ -0,0 +1,47 @@
+import { api } from "~/services/api";
+import EpidemiologyEndpoints from "~/services/epidemiology.endpoints";
+
+class EpidemiologyApi {
+  async getPatients(params) {
+    try {
+      const res = await api.get(EpidemiologyEndpoints.getPatients, { params });
+      return res.data;
+    } catch (error) {
+      throw new Error("Có lỗi xảy ra khi lấy danh sách bệnh nhân.");
+    }
+  }
+
+  async addPatient(patientData) {
+    try {
+      const res = await api.post(EpidemiologyEndpoints.addPatient, patientData);
+      return res.data;
+    } catch (error) {
+      throw new Error("Có lỗi xảy ra khi thêm bệnh nhân.");
+    }
+  }
+
+  async deletePatientsByIds(ids) {
+    try {
+      const res = await api.post(EpidemiologyEndpoints.deletePatientsByIds, {
+        ids,
+      });
+      return res.data;
+    } catch (error) {
+      throw new Error("Có lỗi xảy ra khi xóa bệnh nhân.");
+    }
+  }
+
+  async updatePatient(patientId, patientData) {
+    try {
+      const res = await api.put(
+        `${EpidemiologyEndpoints.updatePatient}?patientId=${patientId}`,
+        patientData
+      );
+      return res.data;
+    } catch (error) {
+      throw new Error("Có lỗi xảy ra khi cập nhật bệnh nhân.");
+    }
+  }
+}
+
+export default new EpidemiologyApi();
---
+++ src/services/epidemiology.endpoints.js
@@ -0,0 +1,8 @@
+const EpidemiologyEndpoints = {
+  getPatients: "/patients",
+  addPatient: "/patients",
+  deletePatientsByIds: "/patients/delete-many",
+  updatePatient: "/patients",
+};
+
+export default EpidemiologyEndpoints;
