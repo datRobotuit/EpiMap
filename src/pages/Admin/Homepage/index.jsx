bac2bbbfeb76b137a3f3ce3de509d0c7b0bc0656
datRobotuit <22520241@gm.uit.edu.vn>
2025-06-12 19:30:28 +0700

    admin homepage fix

--- src/pages/Admin/Homepage/index.jsx
+++ src/pages/Admin/Homepage/index.jsx
@@ -1,7 +1,19 @@
+import MapComponent from "../../../components/MapComponent";
+import DashboardPanel from "../../../components/DashboardPanel";
+import DashboardToggleButton from "../../../components/DashboardToggleButton";
+import { useState } from "react";
+import React from "react";
 export default function AdminHomepage() {
+  const [dashboardOpen, setDashboardOpen] = useState(true);
+  
+      const toggleDashboard = () => {
+          setDashboardOpen(!dashboardOpen);
+      };
   return (
-    <div>
-      <h1>Admin Homepage</h1>
+    <div className="relative min-h-screen bg-gray-100">
+      <MapComponent/>
+      <DashboardPanel isOpen={dashboardOpen} onToggle={toggleDashboard} />
+      <DashboardToggleButton isOpen={dashboardOpen} onToggle={toggleDashboard} />
     </div>
   );
 }
